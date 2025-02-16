74fbd07b5726822a586cde03110db156737a0c06
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-02-15 18:59:41 -0800

    fix json

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,16 +9,16 @@ async fn main() {
     let subscriber = tracing_subscriber::registry().with(
         tracing_subscriber::fmt::layer()
             .compact()
+            // .json()
             .with_file(true)
             .with_line_number(true)
             .with_span_events(FmtSpan::CLOSE)
             .with_target(false)
-            // .json()
             .with_filter(
                 tracing_subscriber::filter::Targets::new()
-                    .with_target("h2", Level::INFO) // filter out h2 logs
-                    .with_target("tower", Level::INFO) // filter out tower debug logs
-                    .with_default(Level::DEBUG),
+                .with_target("h2", Level::INFO) // filter out h2 logs
+                .with_target("tower", Level::INFO) // filter out tower debug logs
+                .with_default(Level::DEBUG),
             ),
     );
 
