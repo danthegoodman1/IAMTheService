2ef99b9b91afc3a840d007a6da6547734b612f32
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-06-03 15:18:18 -0700

    remove registry

--- http_server/aws_service.go
+++ http_server/aws_service.go
@@ -17,30 +17,3 @@ type AWSServiceProvider interface {
 	// Returning a *http.Response will stream that response to the client
 	HandleRequest(ctx context.Context, request *ProxiedRequest) (*http.Response, error)
 }
-
-// AWSServiceRegistry manages the registration and lookup of service providers
-type AWSServiceRegistry struct {
-	providers map[string]AWSServiceProvider
-}
-
-// NewAWSServiceRegistry creates a new registry for AWS service providers
-func NewAWSServiceRegistry() *AWSServiceRegistry {
-	return &AWSServiceRegistry{
-		providers: make(map[string]AWSServiceProvider),
-	}
-}
-
-// RegisterProvider adds a service provider to the registry
-func (r *AWSServiceRegistry) RegisterProvider(provider AWSServiceProvider) {
-	r.providers[provider.ServiceName()] = provider
-}
-
-// GetProviderForRequest finds the appropriate provider for a request
-func (r *AWSServiceRegistry) GetProviderForRequest(request *ProxiedRequest) (AWSServiceProvider, bool) {
-	for _, provider := range r.providers {
-		if provider.CanHandleRequest(request) {
-			return provider, true
-		}
-	}
-	return nil, false
-}
